23875ef594024ca3e39c57ff18ef8646290c56ef
Yulii Shtel <ulik1shtel@gmail.com>
2023-07-11 20:33:07 +0300

    Ternaries Instead of if/else Statements

--- 02-JS-review/script.js
+++ 02-JS-review/script.js
@@ -179,6 +179,10 @@ updatedBook;
 
 const summary = `${title} is ${pages}-page long book, was written by ${author} and published in ${
   publicationDate.split("-")[0]
-}`;
+}. The book has ${hasMovieAdaptation ? "" : "not"} been adapted as a movie`;
 summary;
 console.log(summary);
+
+const pagesRange = pages > 1000 ? "over a thousand" : "less than 1000";
+pagesRange;
+console.log(`The book has ${pagesRange} pages`);
