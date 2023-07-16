5c17b8cc3a3d7d1c6c7e20a0b9ba8e368300c58e
Yulii Shtel <ulik1shtel@gmail.com>
2023-07-16 17:41:09 +0300

    Asynchronous JavaScript: Async/Await

--- 02-JS-review/script.js
+++ 02-JS-review/script.js
@@ -295,8 +295,21 @@ booksAfterUpdate;
 
 */
 
-fetch("https://jsonplaceholder.typicode.com/todos")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
 
-console.log("ulik");
+// console.log("ulik");
+
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+
+  return data;
+}
+
+const todos = getTodos();
+console.log(todos);
+
+console.log("ulik ulik");
