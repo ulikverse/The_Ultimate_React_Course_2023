694f5d8afed1f313b2b1693f7dafb729f2745786
Yulii Shtel <ulik1shtel@gmail.com>
2023-08-07 20:37:51 +0300

    A Vanilla JavaScript Implementation

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -7,6 +7,15 @@ const messages = [
 ];
 
 export default function App() {
+  return (
+    <div>
+      <Steps />
+      <Steps />
+    </div>
+  );
+}
+
+function Steps() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
 
@@ -22,7 +31,7 @@ export default function App() {
   }
 
   return (
-    <>
+    <div>
       <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
@@ -54,6 +63,6 @@ export default function App() {
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 }
